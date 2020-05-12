92366b2c53849e21e0c1168030ca8d36acb95fd4
Vlad Gorokhov <vlad@witchery.io>
2020-05-12 23:11:17 +0400

    Update merge.go

--- merge.go
+++ merge.go
@@ -388,6 +388,11 @@ func mergeFieldListEqual(list1, list2 ast.FieldList) error {
 }
 
 func mergeFieldsEqual(field1, field2 *ast.FieldDefinition) error {
+	
+	if field2 == nil {
+		return nil
+	}
+	
 	// if the 2 descriptions don't match
 	if field1.Description != field2.Description {
 		return fmt.Errorf("conflict in field descriptions. Found \"%v\" and \"%v\"", field1.Description, field2.Description)
